78606497f43c5c5b9680d24c5afea1fce94069b0
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-16 20:22:28 +0700

    fix: atur ukuran dan borderRadius container gambar agar tidak membesar saat gambar diperbesar

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,16 @@ import Button from "@/components/Button";
 
 export default function Index() {
   return (
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <View style={styles.img_container}>
         <Image
           // image by https://www.vecteezy.com/vector-art/23093053-artificial-intelligence-robot-chatbot-vector-ai-chatbot-concept-colored-icon
           source={require('@/assets/images/icon.png')}
           style={styles.img_style}
         />
-        <Button title="Get Started"/>
       </View>
+      <Button title="Get Started" />
+    </View>
   );
 }
 
@@ -21,8 +23,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     fontFamily: "Outfit-Medium"
   },
+  img_container: {
+    width: 250,
+    height: 250,
+    backgroundColor: "#ffdc00",
+    borderRadius: 200,
+    borderWidth: 4,
+    borderColor: '#000000',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   img_style: {
-    width: 300,
-    height: 300
+    width: 250,
+    height: 250,
+    resizeMode: 'contain'
   }
-})
+});
