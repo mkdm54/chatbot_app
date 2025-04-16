19c17ae246f8ffd828dbdfa1b4c8d7ccb28fc5c4
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-16 20:24:47 +0700

    fix : delete fontFamily

--- app/index.tsx
+++ app/index.tsx
@@ -21,7 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    fontFamily: "Outfit-Medium"
   },
   img_container: {
     width: 250,
