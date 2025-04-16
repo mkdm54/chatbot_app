f672a1ee22f6c9c6a2f2f2bcbdbefb026241199b
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-16 10:25:35 +0700

    feat: menambahkan ikon dan teks awal pada tampilan utama

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image
+        // image by https://www.vecteezy.com/vector-art/23093053-artificial-intelligence-robot-chatbot-vector-ai-chatbot-concept-colored-icon
+        source={require('@/assets/images/icon.png')}
+        style={styles.img_style}
+      />
+      <Text>get started</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img_style: {
+    width: 300,
+    height: 300
+  }
+})
