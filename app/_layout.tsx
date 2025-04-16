b9fde54ba23c6191f0101a3a55acd23848e2e4e2
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-16 11:56:20 +0700

    import button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,5 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router/stack';
 
-export default function RootLayout() {
-  return (
-  <Stack>
-    <Stack.Screen
-      name="[profile]"
-    />
-  </Stack>
-  )
-};
+export default function Layout() {
+  return <Stack />;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
 import { Text, View, StyleSheet, Image } from "react-native";
+import Button from "@/components/Button";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Image
-        // image by https://www.vecteezy.com/vector-art/23093053-artificial-intelligence-robot-chatbot-vector-ai-chatbot-concept-colored-icon
-        source={require('@/assets/images/icon.png')}
-        style={styles.img_style}
-      />
-      <Text>get started</Text>
-    </View>
+      <View style={styles.container}>
+        <Image
+          // image by https://www.vecteezy.com/vector-art/23093053-artificial-intelligence-robot-chatbot-vector-ai-chatbot-concept-colored-icon
+          source={require('@/assets/images/icon.png')}
+          style={styles.img_style}
+        />
+        <Button title="Get Started"/>
+      </View>
   );
 }
 
@@ -18,6 +19,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    fontFamily: "Outfit-Medium"
   },
   img_style: {
     width: 300,
