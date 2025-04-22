623f671201f77bd49a6f958849ecf9cf0742edb3
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-22 10:01:45 +0700

    feat : tambah color constant untuk text

--- constant/Color.tsx
+++ constant/Color.tsx
@@ -1,8 +1,10 @@
+// NEOBRUTALISM COLOR
 export const Colors = {
   light: {
     yellow_background: "#ffD850",
     green_background: "#90EE90",
     border_color: "#6a6054",
     shadow_color: "#ccc8c5",
+    text: "#6a6054",
   },
 };
