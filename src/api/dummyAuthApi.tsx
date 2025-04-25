431e52593942da932b6ae82c21812da5430de30e
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-25 10:47:43 +0700

    feat: tambah pemanggilan API login untuk menangani autentikasi pengguna

---
+++ src/api/dummyAuthApi.tsx
@@ -0,0 +1,24 @@
+export const login = async (username: string, password: string) => {
+  try {
+    const response = await fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+        expiresInMins: 30,
+      }),
+      credentials: "include",
+    });
+
+    if (!response.ok) {
+      throw new Error("Login failed");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error logging in:", error);
+    return { error: "Login failed" };
+  }
+};
