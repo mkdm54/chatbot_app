adc24560e35a471734bdbaf7ef2f1effa3bb97f4
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-25 22:32:25 +0700

    style : hilangkan header pada halaman not found

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,6 +25,12 @@ export default function Layout() {
           headerShown: false,
         }}
       />
+      <Stack.Screen
+        name="not-found"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
