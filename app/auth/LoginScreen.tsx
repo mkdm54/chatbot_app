1e69973ccc6a464b8b8bdeebdb2316811c2d4584
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-25 22:34:05 +0700

    update : mengubah Text login menjadi Masuk

--- app/auth/LoginScreen.tsx
+++ app/auth/LoginScreen.tsx
@@ -124,7 +124,7 @@ export default function LoginScreen() {
         </View>
 
         <Button
-          title="Login"
+          title="Masuk"
           style={styles.button_component_style}
           onPress={handleLogin}
         />
