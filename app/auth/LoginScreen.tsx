f6a8b8e084feffeb334211001e62eba012c258d9
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-25 21:38:09 +0700

    style : memperbaiki posisi text login

--- app/auth/LoginScreen.tsx
+++ app/auth/LoginScreen.tsx
@@ -174,7 +174,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     textAlign: "center",
     width: "100%",
-    height: 70,
+    height: 75,
     top : 20, 
   },
   img_style: {
