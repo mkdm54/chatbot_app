ea41088a2e8695faa9bb0fa7efb5a812915afe3d
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-25 22:10:58 +0700

    update : tambah warna merah

--- constant/Color.tsx
+++ constant/Color.tsx
@@ -1,6 +1,7 @@
 // NEOBRUTALISM COLOR
 export const Colors = {
   light: {
+    red : '#FF4910',
     yellow_background: "#ffD850",
     aqua_background : "#7DFAFF",
     green_background: "#90EE90",
