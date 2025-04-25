ab6d1f6ed34f9bdff90611e2b4ef97db9342f39d
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-25 20:19:16 +0700

    feat : tambahkan kode warna #7DFAFF pada color constant

--- constant/Color.tsx
+++ constant/Color.tsx
@@ -2,6 +2,7 @@
 export const Colors = {
   light: {
     yellow_background: "#ffD850",
+    aqua_background : "#7DFAFF",
     green_background: "#90EE90",
     border_color: "#6a6054",
     shadow_color: "#ccc8c5",
