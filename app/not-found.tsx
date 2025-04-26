887a168f94aecce70ee3ec8a24d1599e76426b77
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-26 09:31:57 +0700

    style : tambahkan deskripsi code 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,11 @@ export default function NotFoundScreen() {
 
   return (
     <View style={styles.container}>
-      <Image source={require("@/assets/images/404.png")} style={[styles.image, {tintColor: Colors.light.red}]} />
+      <Image
+        source={require("@/assets/images/404.png")}
+        style={[styles.image, { tintColor: Colors.light.red_v1 }]}
+      />
+      <Text style={styles.code_404}>404</Text>
       <Text style={styles.title}>Oops! Halaman tidak ditemukan</Text>
       <Text style={styles.description}>
         Halaman yang kamu cari tidak tersedia atau sudah dipindahkan.
@@ -39,26 +43,36 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 26,
-    fontWeight: "bold",
     color: Colors.light.text,
     textAlign: "center",
     marginBottom: 10,
+    fontFamily: "Outfit-Bold",
   },
   description: {
     fontSize: 16,
     color: Colors.light.text,
     textAlign: "center",
     marginBottom: 30,
+    fontFamily: "Outfit-Medium",
   },
   button: {
-    backgroundColor: Colors.light.border_color,
+    borderWidth: 4,
+    borderColor: Colors.light.border_color,
+    backgroundColor: Colors.light.red_v1,
     paddingVertical: 12,
     paddingHorizontal: 30,
     borderRadius: 10,
   },
   buttonText: {
     color: "#fff",
-    fontSize: 16,
+    fontSize: 20,
     fontFamily: "Outfit-Medium",
   },
+  code_404 : {
+    fontSize: 40,
+    color: Colors.light.red_v1,
+    textAlign: "center",
+    marginBottom: 10,
+    fontFamily: "Outfit-Bold",
+  }
 });
