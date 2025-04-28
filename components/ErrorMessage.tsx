8943ef92b33d88f3b23238b019933c4d4f8cb9dc
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-04-28 13:11:08 +0700

    update : mengubah font erro message ke `Outfit-Medium`

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -1,12 +1,27 @@
+import { useEffect } from "react";
 import { View, Text } from "react-native";
+import useCustomFonts from "@/hooks/useCustomFonts";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 interface MessageProops {
   message?: string;
 }
 
 const ErrorMessage = ({ message }: MessageProops) => {
-  if (!message) return null;
+  const [loaded, error] = useCustomFonts();
 
+  if (!message) return null;
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <View
       style={{
@@ -14,11 +29,11 @@ const ErrorMessage = ({ message }: MessageProops) => {
         padding: 10,
         borderRadius: 5,
         marginVertical: 5,
-        alignItems : "center",
-        justifyContent: "center"
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      <Text style={{ color: "#cc0000", fontWeight: "bold" }}>{message}</Text>
+      <Text style={{ color: "#cc0000", fontWeight: "bold", fontFamily: "Outfit-Medium" }}>{message}</Text>
     </View>
   );
 };
